7e95b53a96f6f0d24d36b08de9fd4c315b784e81
Albert Padin <albertpadin@gmail.com>
2015-06-15 11:46:58 +0800

    Add gaesentry

---
+++ gaesentry/__init__.py
@@ -0,0 +1,17 @@
+import os
+from raven import Client
+from raven.handlers.logging import SentryHandler
+from raven.conf import setup_logging
+import logging
+
+
+GAESENTRY_SENTRY_DSN = ''  # get this from getsentry
+
+
+def add_sentry_to_logging():
+    if not os.environ.get('HTTP_HOST').startswith('localhost:'):
+        # Configure the default client
+        client = Client(GAESENTRY_SENTRY_DSN)
+        handler = SentryHandler(client=client, level=logging.WARNING)
+        setup_logging(handler)
+
